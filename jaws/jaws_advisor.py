cf503456b8f0b7cecb92e7fcf32daa1a66d4c002
Derek <contact@derekburgess.com>
2024-05-16 08:45:32 -0400

    Adjustments

--- jaws/jaws_advisor.py
+++ jaws/jaws_advisor.py
@@ -8,7 +8,6 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from openai import OpenAI
 import base64
-import requests
 
 
 uri = os.getenv("NEO4J_URI")
@@ -19,27 +18,32 @@ client = OpenAI()
 jaws_finder_endpoint = os.getenv("JAWS_FINDER_ENDPOINT")
 image_to_encode = f"{jaws_finder_endpoint}pca_dbscan_outliers.png"
 system_prompt = """
-As an expert IT Professional, Sysadmin, and Analyst, you are tasked with reviewing logs and reports of networking traffic to identify patterns and suggest improvements for firewall configurations. Please analyze the provided network traffic data and make recommendations for enhancing firewall security, returning a brief report in the following format:
+You are an expert IT Professional, Sysadmin, and Analyst. Your task is to review data from network traffic to identify patterns and make recommendations for firewall configurations. Please analyze the provided network traffic and cluster plot, then return a brief report in the following format:
 
-Traffic Analysis:
+---
 
-1. Identify common traffic patterns. Summarize these using an ASCII based diagrammatic notation that includes organizations, hostnames, IP addresses, port numbers, and traffic size (e.g., ( org, hostname ) - [ ownership ] -> ( src_ip:src_port ) - [ packet size ] -> ( dst_ip:dst_port ).
+### Executive Summary
 
-2. Highlight any anomalies or unusual patterns.
+A concise summary of the traffic analysis, including a description of the cluster plot.
 
-Firewall Recommendations:
+### Traffic Analysis
 
-1. Based on the traffic patterns identified, list detailed recommendations for enhancing firewall security. Each recommendation should refer directly to specific addresses, ports, or domains observed in the dataset.
+1. **Common Traffic Patterns**: Identify and describe the regular traffic patterns. Highlight any anomalies or unusual patterns.
+   
+2. **Network Diagram**: Create an ASCII-based diagram that illustrates the network. Include organizations, hostnames, IP addresses, port numbers, and traffic size.
 
-2. Provide a rationale for each recommendation, explaining why it addresses a specific issue identified in the traffic analysis.
+### Firewall Recommendations
 
-Additional Instructions:
+1. **Recommendations**: List detailed recommendations for enhancing firewall security based on the traffic patterns identified.
+   
+2. **Rationale**: Provide a rationale for each recommendation, explaining how it addresses specific issues identified in the traffic analysis.
 
-- Begin with an executive summary of the traffic analysis.
+### Additional Instructions
+
 - Use clear, concise language.
 - Utilize ASCII diagrams to represent traffic flows effectively.
 - Ensure recommendations are specific and supported by data from the provided logs.
-- Avoid too much formatting.
+- Avoid excessive formatting.
 """
 
 
@@ -72,15 +76,12 @@ def fetch_data(driver, database):
                 'location': record['location']
             })
         df = pd.DataFrame(data)
-        print(f"\nSending {df.shape[0]} outliers (snapshot below):", "\n")
-        print(df.head(), "\n")
         df_json = df.to_json(orient="records")
-        return df_json
+        return df, df_json
     
 
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
-    print(f"\nEncoding and sending image from: {image_path}")
     return base64.b64encode(image_file.read()).decode('utf-8')
 base64_image = encode_image(image_to_encode)
 
@@ -122,7 +123,7 @@ class SummarizeOpenAI:
     def __init__(self, client):
         self.client = client
         self.model_name = "gpt-4o"
-
+    
     def generate_summary_from_openai(self, system_prompt, df_json):
         completion = self.client.chat.completions.create(
             model=self.model_name,
@@ -160,13 +161,17 @@ def main():
                         help="Specify the Neo4j database to connect to (default: captures)")
 
     args = parser.parse_args()
-
-    df_json = fetch_data(driver, args.database)
+    df, df_json = fetch_data(driver, args.database)
 
     if args.api == "transformers":
+        print(f"\nSending {df.shape[0]} outliers (snapshot below):", "\n")
+        print(df.head(), "\n")
         transformer = SummarizeTransformers()
         transformer.generate_summary_from_transformers(system_prompt, df_json)
     elif args.api == "openai":
+        print(f"\nEncoding and sending image from: {image_to_encode}")
+        print(f"\nSending {df.shape[0]} outliers (snapshot below):", "\n")
+        print(df.head(), "\n")
         transformer = SummarizeOpenAI(client)
         transformer.generate_summary_from_openai(system_prompt, df_json)
     else:
